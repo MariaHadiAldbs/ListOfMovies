77b9688d18e74b81a465ba8c237442cdbe6be7c0
MariaHadiAldbs <110459573+MariaHadiAldbs@users.noreply.github.com>
2022-08-07 13:57:23 +0400

    FakeMoviesTable
    each movie has a name/genre/price/rating 
    and has a delete button that updates the massage on top that explains the number of movies in the table

---
+++ App.js
@@ -0,0 +1,72 @@
+function DieHard() {
+  document.getElementById("DieHard").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 2);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 2
+  } movies in the list`;
+}
+function myBestFriendsWedding() {
+  document.getElementById("myBestFriendsWedding").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 3);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 3
+  } movies in the list`;
+}
+function theIntern() {
+  document.getElementById("theIntern").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 4);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 4
+  } movies in the list`;
+}
+function Avengers() {
+  document.getElementById("Avengers").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 5);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 5
+  } movies in the list`;
+}
+function theFaultInOurStars() {
+  document.getElementById("theFaultInOurStars").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 6);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 6
+  } movies in the list`;
+}
+function lairLair() {
+  document.getElementById("lairLair").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 7);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 7
+  } movies in the list`;
+}
+function theFounder() {
+  document.getElementById("theFounder").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 8);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 8
+  } movies in the list`;
+}
+function twilight() {
+  document.getElementById("twilight").innerHTML = "";
+  var x = document.getElementById("table").rows.length;
+  console.log(x - 9);
+  document.getElementById("h4").innerHTML = `there are ${
+    x - 9
+  } movies in the list`;
+}
+function theTwin() {
+  document.getElementById("theTwin").innerHTML = "";
+  document.getElementById("heading").innerHTML = "";
+
+  var x = document.getElementById("table").rows.length;
+  console.log(0);
+  document.getElementById("h4").innerHTML = "there are no movies in the list";
+}
